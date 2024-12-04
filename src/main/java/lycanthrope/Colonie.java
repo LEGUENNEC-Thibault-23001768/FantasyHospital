a260a1bf218a58f86365a716b4f5403fb469fbd9
SEBAA Ayrton <157604336+SEBAA-Ayrton-2326097c@users.noreply.github.com>
2024-12-04 21:19:57 +0100

    Update Colonie.java

--- src/main/java/lycanthrope/Colonie.java
+++ src/main/java/lycanthrope/Colonie.java
@@ -3,18 +3,13 @@ package lycanthrope;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Classe représentant une colonie de lycanthropes.
- */
 public class Colonie {
     private List<Meute> meutes;
 
-    // Constructeur
     public Colonie() {
         meutes = new ArrayList<>();
     }
 
-    // Méthodes
     public void ajouterMeute(Meute meute) {
         meutes.add(meute);
     }
@@ -27,8 +22,6 @@ public class Colonie {
     }
 
     public void simuler() {
-        // Simulation d'événements
         System.out.println("Début de la simulation...");
-        // Exemples : reproduction, hurlements, transformations
     }
 }
